75754e6a723d61925363c918368b87757b433a4c
Kevin Colgan <kevin@civic.com>
2019-01-28 10:26:53 +0100

    initial tests for Validation process classes

---
+++ src/vp/ValidationProcess.js
@@ -0,0 +1,128 @@
+const _ = require('lodash');
+const { definitions, UserCollectableAttribute } = require('@identity.com/uca');
+const { ValidationProcessStatus } = require('../constants/validationConstants');
+
+const validIdentifiers = definitions.map(d => d.identifier);
+
+const defaultUcaVersion = '1';
+class BadUCAValueError extends Error {
+  constructor(ucaId, value, error) {
+    const errMessage = `BadUCAValue: UCA value '${JSON.stringify(value)}' isn't good for UCA Identifier '${ucaId}' UCA error = ${error}`;
+    super(errMessage);
+    this.name = 'BadUCAValue';
+    this.ucaId = ucaId;
+    this.value = value;
+  }
+}
+class BadValidationProcessError extends Error {
+  constructor(error) {
+    super(error);
+    this.name = 'BadValidationProcessError';
+  }
+}
+class BadValidationUCAError extends Error {
+  constructor(error) {
+    super(error);
+    this.name = 'BadValidationUCAError';
+  }
+}
+
+class ValidationUCAValue {
+  constructor(name, value, ucaVersion) {
+    this.name = name;
+    this.ucaVersion = ucaVersion;
+    this.setValue(value);
+  }
+
+  setValue(value) {
+    // check that the input value is valid for this type of UCA
+    if (this.name && _.includes(validIdentifiers, this.name)) {
+      // instantiate a UCA to check the value
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const ucaObject = new UserCollectableAttribute(this.name, value, this.ucaVersion);
+      } catch (error) {
+        throw new BadUCAValueError(this.name, value, error);
+      }
+    }
+    this.value = value;
+  }
+
+  // value can be any type, including an object
+  serialize() {
+    return {
+      value: this.value,
+    };
+  }
+}
+
+class ValidationUCA {
+  
+  constructor(ucaMapId, ucaObj, ucaVersion = '1', dependsOnStatus) {
+    const getOrThrow = (obj, key) => {
+      const retVal = _.get(obj, key);
+      if(retVal) return retVal;
+      throw new BadValidationUCAError(`${key} not present in ${obj}`);
+    };
+    // the ucaMapId is the string reference used in the process to refer to this UCA
+    // it is also used in the URL for patching values to this UCA
+    this.ucaMapId = ucaMapId;
+    this.ucaName = getOrThrow(ucaObj, 'name');
+    this.status = getOrThrow(ucaObj, 'status');
+    this.ucaVersion = ucaVersion;
+    this.dependsOnStatus = dependsOnStatus;
+    this.dependsOn = _.get(ucaObj, 'dependsOn', []);
+  }
+
+  get url() {
+    return `/ucas/${this.ucaMapId}`;
+  }
+
+  getValueObj(value) {
+    // ValidationUCAValue will throw an error if the value isn't good for the UCA type
+    const validationUCAValueInst = new ValidationUCAValue(this.ucaName, value, this.ucaVersion);
+    return validationUCAValueInst.serialize();
+  }
+
+  get dependsOnArray() {
+    if (!this.dependsOnValidationUcas && this.dependsOn && this.dependsOn.length > 0) {
+      // eslint-disable-next-line no-unused-vars, no-undef
+      this.dependsOnValidationUcas = _.map(this.dependsOn, (dependsOnObj) => new ValidationUCA(null, dependsOnObj.uca, this.ucaVersion, dependsOnObj.status));
+    }
+    return this.dependsOnValidationUcas;
+  }
+}
+
+class ValidationProcess {
+  
+  constructor(processObj) {
+    const getOrThrow = (obj, key) => {
+      const retVal = _.get(obj, key);
+      if(retVal) return retVal;
+      throw new BadValidationProcessError(`${key} not present in ${obj}`);
+    }
+    this.id = getOrThrow(processObj, 'id');
+    this.credentialItem = getOrThrow(processObj, 'state.credential');
+    this.processUrl = getOrThrow(processObj, 'processUrl');
+    this.status = getOrThrow(processObj, 'state.status');
+    this.ucaVersion = getOrThrow(processObj, 'state.ucaVersion');
+    this.ucas = getOrThrow(processObj, 'state.ucas');
+  }
+
+  getValidationUcas() {
+    if (!this.validationUcas) {
+      // eslint-disable-next-line no-unused-vars, no-undef
+      this.validationUcas = _.map(Object.entries(this.ucas), ([ucaId, ucaObj]) => new ValidationUCA(this.ucaMapId, ucaObj, this.ucaVersion));
+    }
+    return this.validationUcas;
+  }
+
+  getValidationUcasByStatus(status) {
+    console.log(`getValidationUcasByStatus(${status})`);
+    return _.filter(this.getValidationUcas(), vUca => (vUca.status === status));
+  }
+}
+
+module.exports = {
+  ValidationProcess, ValidationUCA, ValidationUCAValue, BadUCAValueError,
+};
--- test/unit/vp/ValidationProcess.test.js
+++ test/unit/vp/ValidationProcess.test.js
@@ -1,11 +1,123 @@
 
+const _ = require('lodash');
+const { 
+  validationProcessInitialState, 
+  validationProcessOneAwaitingUserInputTwoAccepted,
+  validUCAObj,
+  noStatusUCAObj,
+  noNameUCAObj,
+  nameUcaVal,
+  phoneNumberToken } = require('../../fixtures/validationProcess');
+const { 
+  ValidationProcess, 
+  BadValidationProcessError, 
+  ValidationUCA,
+  BadValidationUCAError } = require('../../../src/vp/ValidationProcess');
+const { UCAStatus } = require('../../../src/constants/validationConstants');
 
 describe('ValidationProcess', () => {
-  beforeAll(() => {
+  it('parse valid Validation Process', () => {
+    /*
+    this.id = _.get(processObj, 'id');
+    this.credentialItem = _.get(processObj, 'state.credentialItem');
+    this.processUrl = _.get(processObj, 'processUrl');
+    this.status = _.get(processObj, 'state.status', ValidationProcessStatus.IN_PROGRESS);
+    this.ucaVersion = _.get(processObj, 'state.ucaVersion', defaultUcaVersion);
+    this.ucas = _.get(processObj, 'state.ucas', {});
+    */
+    const parsedVP = new ValidationProcess(validationProcessInitialState);
+    expect(parsedVP).toBeDefined();
+    console.log(JSON.stringify(parsedVP, null, 2));
+
+    expect(parsedVP.id).toBeDefined();
+    expect(parsedVP.credentialItem).toBeDefined();
+    expect(parsedVP.processUrl).toBeDefined();
+    expect(parsedVP.status).toBeDefined();
+    expect(parsedVP.ucaVersion).toBeDefined();
+    expect(parsedVP.ucas).toBeDefined();
   });
 
-  it('test 1', () => {
+  it('throws an error with an invalid Validation Process', () => {
+    const necessaryProperties = ['id', 'state.credential', 'processUrl', 'state.status', 'state.ucaVersion', 'state.ucas'];
+    necessaryProperties.forEach(element => {
+      let invalidationProcessInitialState = _.merge({}, validationProcessInitialState);
+      _.unset(invalidationProcessInitialState, element);
+      const expextToFail = () => new ValidationProcess(invalidationProcessInitialState);
+      expect(expextToFail).toThrowError(BadValidationProcessError);
+    });
+  });
 
-    expect(true).to.equal(true);
+  it('getValidationUcas returns an array of ValidationUCA objects', () => {
+    const parsedVP = new ValidationProcess(validationProcessInitialState);
+    expect(parsedVP).toBeDefined();
+
+    expect(parsedVP.getValidationUcas()).toBeDefined();
+    console.log(JSON.stringify(parsedVP.getValidationUcas(), null, 2));
+  });
+
+  it('getValidationUcasByStatus returns the correct number of UCAs', () => {
+    const parsedVP = new ValidationProcess(validationProcessOneAwaitingUserInputTwoAccepted);
+    expect(parsedVP).toBeDefined();
+    console.log(JSON.stringify(parsedVP, null, 2));
+
+    expect(parsedVP.getValidationUcasByStatus(UCAStatus.ACCEPTED).length).toEqual(2);
+    expect(parsedVP.getValidationUcasByStatus(UCAStatus.AWAITING_USER_INPUT).length).toEqual(1);
+  });
+});
+
+describe.only('ValidationUCA', () => {
+  it('parses a valid ValidationUCA', () => {
+    /*
+    this.ucaMapId = ucaMapId;
+    this.ucaName = getOrThrow(ucaObj, 'name');
+    this.status = getOrThrow(ucaObj, 'status');
+    this.dependsOn = _.get(ucaObj, 'dependsOn');
+    this.ucaVersion = ucaVersion;
+    */
+    const parsedValidationUCA = new ValidationUCA('test', validUCAObj, '1');
+    expect(parsedValidationUCA).toBeDefined();
+    console.log(JSON.stringify(parsedValidationUCA, null, 2));
+
+    expect(parsedValidationUCA.ucaMapId).toBeDefined();
+    expect(parsedValidationUCA.ucaName).toBeDefined();
+    expect(parsedValidationUCA.status).toBeDefined();
+    expect(parsedValidationUCA.dependsOn).toBeDefined();
+    expect(parsedValidationUCA.ucaVersion).toBeDefined();
+  });
+
+  it('throws an error with no status in ValidationUCA', () => {
+    const expextToFail = () => new ValidationUCA('test', noStatusUCAObj, '1');
+      expect(expextToFail).toThrowError(BadValidationUCAError);
+  });
+
+  it('throws an error with no name in ValidationUCA', () => {
+    const expextToFail = () => new ValidationUCA('test', noNameUCAObj, '1');
+      expect(expextToFail).toThrowError(BadValidationUCAError);
+  });
+
+  describe('with a valid ValidationUC', () => {
+    it('returns a valid URL value', () => {
+      const parsedValidationUCA = new ValidationUCA('test', validUCAObj, '1');
+      expect(parsedValidationUCA.url).toBeDefined();
+      expect(parsedValidationUCA.url).toEqual('/ucas/test');
+    });
+
+    it('returns a valid value object', () => {
+      const parsedValidationUCA = new ValidationUCA('test', validUCAObj, '1');
+      const expVal = { value: nameUcaVal };
+      expect(parsedValidationUCA.getValueObj(nameUcaVal)).toEqual(expVal);
+    });
+
+    it('returns a valid dependsOn array', () => {
+      const parsedValidationUCA = new ValidationUCA('phoneNumberToken', phoneNumberToken, '1');
+      const expDependsOnArray = [
+        new ValidationUCA(null, { "name": "cvc:PhoneNumber:countryCode",
+          "status": "AWAITING_USER_INPUT"}, '1', "VALIDATING"),
+          new ValidationUCA(null, { "name": "cvc:PhoneNumber:number",
+          "status": "AWAITING_USER_INPUT"}, '1', "VALIDATING"),
+      ];
+      console.log(`parsedValidationUCA.dependsOnArray = ${JSON.stringify(parsedValidationUCA.dependsOnArray)}`);
+      expect(parsedValidationUCA.dependsOnArray).toEqual(expDependsOnArray);
+    });
   });
 });
