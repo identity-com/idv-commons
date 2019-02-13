8428611871f4b345d663c5be41360a250008cb97
Bruno Carneiro <brunoac88@gmail.com>
2019-02-13 13:31:16 -0200

    CCS-864 - Exporting vp folder

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,11 +1,19 @@
-const i = require('../../src');
+const idvCommons = require('../../src');
 
 describe('index.js test', () => {
   it('Should instanciate', () => {
-    const { CredentialRequestManager, CredentialRequest, Constants } = i;
+    const {
+      CredentialRequestManager,
+      CredentialRequest,
+      Constants,
+      ValidationProcess,
+      ValidationErrors,
+    } = idvCommons;
     expect(CredentialRequestManager).toBeDefined();
     expect(CredentialRequest).toBeDefined();
     expect(Constants.ValidationProcessStatus).toBeDefined();
     expect(Constants.UCAStatus).toBeDefined();
+    expect(ValidationProcess).toBeDefined();
+    expect(ValidationErrors).toBeDefined();
   });
 });
